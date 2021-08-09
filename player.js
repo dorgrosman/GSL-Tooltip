2573d2df1c7d8bcde83792d5d15681b51b6ab4fb
dorgrosman <ddorgrosman@gmail.com>
2021-08-09 17:34:45 +0300

    17:30 09/08/21 - popup's close and 'NEXT' btn work

--- player.js
+++ player.js
@@ -27,9 +27,11 @@ function createTooltip() {
 }
 
 function updateTiplate(tiplate) {
-//     const elPopoverInner = elTooltip.querySelector('.popover-inner')
     const elPopoverInner = elTooltip.querySelector('.popover-inner')
     elPopoverInner.innerHTML = tiplate
+    if(index == 4 ){
+        elPopoverInner.innerHTML.removeChild(elPopoverInner)
+    }
     
     return elTooltip
 }
@@ -37,19 +39,91 @@ function updateTiplate(tiplate) {
 injectCSSLink()
 injectJSONPScript()
 const elTooltip = createTooltip()
+var index = 0 ;
+var closeBtn = true ;
+
 
 window.__5szm2kaj = function (details) {
-    var index = '0' ;
-    const staps = (details.data.structure.steps)
-    var currentStep =staps[index]
-    console.log('currentStep',currentStep)
+
+    var staps = (details.data.structure.steps)
+    
+
+    var currentStep = staps[index]     
+    var updated = updateTiplate(details.data.tiplates.tip)             
+    var content = updated.querySelector('[data-iridize-id="content"]').innerHTML = currentStep.action.contents['#content']    
+    var prevBt = updated.querySelector('[data-iridize-role="prevBt"]').innerHTML //dosnt prestn me the Back btn
+    
+    var popupPosiTop = document.querySelector('.sttip').style.top ="50%"
+    var popupPosiLeft = document.querySelector('.sttip').style.left ="50%"
+
+    var stepCounter =updated.querySelector('[data-iridize-role="stepCount"]').innerText = ` ${index + 1}`
+    var stepsCounter =updated.querySelector('[data-iridize-role="stepsCount"]').innerText = ` 4`
+    
+    var nextBt = updated.querySelector('[data-iridize-role="nextBt"]').addEventListener("click",function(){
+//     var popupPosi = updated.querySelector('.k1zIA').setAttribute("style","margin-top: 60px")
+//     var popupPosi = updated.querySelector('.k1zIA').style.left ='-120px'
+//      var popupPosi = updated.querySelector('.sttip').setAttribute("style","left: 60px")
+     
+    
+    if(index ==  0){
+       
+    }
+
+    stepCounter =updated.querySelector('[data-iridize-role="stepCount"]').innerText = ` ${index + 2}`
+
+        if(index == 3){
+         updateTiplate(null)
+         
+             return 
+         }else{
+
+          
         
-    var updated = updateTiplate(details.data.tiplates.tip)
-    var nextBt = updated.querySelector("[data-iridize-role=\"nextBt\"]")
+         index = index + 1;
+         currentStep = staps[index]
+         if(index != '0' )
+//          
+         content = updated.querySelector('[data-iridize-id="content"]').innerHTML = currentStep.action.contents['#content']
 
+         document.body.appendChild(elTooltip);
+     } })
 
+    var closePop = updated.querySelector('[data-iridize-role="closeBt"]').addEventListener("click",function(){
+        console.log('hii')
+        closeBtn = false;
+        updateTiplate(null)
+        console.log('closeBtn',closeBtn)
+//         node.appendChild(node)
 
-document.body.appendChild(elTooltip);
+    })
+//   const trem1 = updated.querySelector('[data-iridize-id="content"]').innerText ='Hi Dor'
 
+//     const staps = (details.data.structure.steps).map(stap=>{
+
+//         if(stap.id == stapNum){
+//        switch(stap.id){
+//     case '1':
+//       const title = updated.querySelector('[data-iridize-role="title"]').innerHTML = stap.action.contents['#content']
+//         break;
+//         case '3':
+//      const titl3 = updated.querySelector('[data-iridize-role="title"]').innerHTML = stap.action.contents['#content']
+//         break;
+//         case '4':
+//      const titl4 = updated.querySelector('[data-iridize-role="title"]').innerHTML = stap.action.contents['#content']
+//         break;
+//         case '5':
+//      const titl5 = updated.querySelector('[data-iridize-role="title"]').innerHTML = stap.action.contents['#content']
+//         break;
+//          case 'eol0':
+// //     const tremeol0 = updated.querySelector('[data-iridize-role="title"]').innerHTML = stap.action.contents['#content']
+//         break;
+//      break;
+//      default:
+
+//     }}})
+//     console.log('elTooltip',elTooltip)
+// document.body.appendChild(elTooltip);
+//     document.body.appendChild(elTooltip);
 
 }
+document.body.appendChild(elTooltip);
